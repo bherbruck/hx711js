320b0903c316111ef3e507ac1c4f0561d0982527
bherbruck <brennen.herbruck@gmail.com>
2021-06-02 23:20:34 -0400

    refactor... everything

--- src/hx711.ts
+++ src/hx711.ts
@@ -4,99 +4,77 @@ function avg(values: number[]) {
   return values.reduce((pre, cur) => pre + cur) / values.length
 }
 
+function wait(ms: number) {
+  // hacky synchronous wait
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms)
+}
+
 export class HX711 {
   private clockGpio: Gpio
   private dataGpio: Gpio
-  private isReading = false
 
   constructor(
     public clockPin: number,
     public dataPin: number,
-    public gain: 128 | 64 | 32 = 128,
     public scale = 1,
     public offset = 0
   ) {
     this.clockGpio = new Gpio(clockPin, 'out')
-    this.dataGpio = new Gpio(dataPin, 'in', 'both')
-    this.powerUp()
-  }
+    this.dataGpio = new Gpio(dataPin, 'in')
 
-  get gainBits() {
-    return { 128: 1, 64: 3, 32: 2 }[this.gain]
+    this.readRawAvg() // read flush things out
   }
 
   get isReady() {
-    return this.dataGpio.readSync() == 0 && !this.isReading
-  }
-
-  private readBit() {
-    this.clockGpio.writeSync(1)
-    const value = this.dataGpio.readSync()
-    this.clockGpio.writeSync(0)
-    return value
-  }
-
-  private pulse(times: number) {
-    let value = 0
-    for (let i = 0; i < times; i++) {
-      value <<= 1
-      value |= this.readBit()
-    }
-    return value
-  }
-
-  private readByte() {
-    return this.pulse(8)
-  }
-
-  private readRawBytes() {
-    while (!this.isReady) {}
-    this.isReading = true
-    // read three bytes (32 bits)
-    const byte1 = this.readByte()
-    const byte2 = this.readByte()
-    const byte3 = this.readByte()
-    // set gain bits for next reading
-    for (let i = 0; i < this.gainBits; i++) {
-      this.readByte()
-    }
-    this.isReading = false
-    return [byte1, byte2, byte3]
-  }
-
-  private readRaw() {
-    const bytes = this.readRawBytes()
-    const filter = bytes[0] & 0x80 ? 0xff : 0x00
-    const value = (filter << 24) | (bytes[0] << 16) | (bytes[1] << 8) | bytes[2]
-    return value + this.offset
-  }
-
-  public readRawAvg(times = 10) {
-    const readings: number[] = []
-    for (let i = 0; i < times; i++) {
-      readings.push(this.readRaw())
-    }
-    return avg(readings)
-  }
-
-  public read() {
-    return this.readRaw() / this.scale
-  }
-
-  public readAvg(times = 10) {
-    return this.readRawAvg(times) / this.scale
-  }
-
-  public tare(times = 10) {
-    this.offset = -this.readRawAvg(times)
+    return this.dataGpio.readSync() == 0
   }
 
   public powerUp() {
     this.clockGpio.writeSync(0)
+    wait(1)
   }
 
   public powerDown() {
-    this.clockGpio.writeSync(0)
     this.clockGpio.writeSync(1)
+    wait(1)
+  }
+
+  private readRaw() {
+    // wrapping in powerUp and powerDown seem to make data much
+    // more stable at the cost of cpu usage and read frequency
+    this.powerUp()
+
+    while (!this.isReady) {}
+
+    let data = 0
+
+    for (let i = 0; i < 24; i++) {
+      this.clockGpio.writeSync(1)
+      this.clockGpio.writeSync(0)
+      data <<= 1
+      data |= this.dataGpio.readSync()
+    }
+
+    data ^= 0x800000
+
+    this.powerDown()
+
+    return data
+  }
+
+  private readRawAvg(times: number = 10) {
+    const values: number[] = []
+    for (let i = 0; i < times; i++) {
+      values.push(this.readRaw())
+    }
+    return avg(values)
+  }
+
+  public read() {
+    return (this.readRaw() + this.offset) / this.scale
+  }
+
+  public tare() {
+    this.offset = -this.readRawAvg()
   }
 }
